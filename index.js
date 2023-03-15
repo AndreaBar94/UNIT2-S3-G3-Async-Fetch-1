551fd1af90907e8a25fb71225ac3119f66016b61
AndreaBar94 <122469003+AndreaBar94@users.noreply.github.com>
2023-03-15 16:20:35 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,70 @@
+fetch("https://striveschool-api.herokuapp.com/books")
+	.then((responseObj) => responseObj.json())
+	.then((books) => {
+		const box = document.getElementById("box");
+		box.innerHTML = "";
+		const row = document.createElement("div");
+		row.className = "row";
+		row.classList.add("row-cols-4");
+		row.classList.add("d-flex");
+		row.classList.add("align-items-end");
+		row.classList.add();
+		row.classList.add("g-5");
+		row.classList.add("mt-3");
+		box.appendChild(row);
+
+		books.forEach((book) => {
+			let col = document.createElement("div");
+			col.className = "col";
+			col.innerHTML = `
+            <div class="card shadow" id="${book.asin}">
+            <img src="${book.img}" class="card-img-top" alt="book thumbnail">
+            <div class="card-body">
+            <h5 class="card-title">${book.title}</h5>
+            <p class="card-text">${book.category}; Price: ${book.price}</p>
+            <button type="button" id="add-to-cart" class="btn btn-primary">Compra ora</button>
+            <button type="button" id="discard-btn" class="btn btn-primary">Scarta</button>
+            </div>
+        </div>
+                        `;
+			//elimina la card
+			let deleteBtn = col.querySelector("#discard-btn");
+			deleteBtn.addEventListener("click", () => {
+				col.remove();
+			});
+
+			//aggiungi al carrello
+
+			let addToCart = col.querySelector("#add-to-cart");
+			let cartList = document.querySelector("#cart-list");
+			addToCart.addEventListener("click", () => {
+				let item = document.createElement("li");
+				item.className = "dropdown-item";
+				item.innerText = `${book.title}`;
+				cartList.appendChild(item);
+				//salvo nel local storage
+				let CART = "Cart";
+				let cart = [];
+				if (localStorage.getItem(CART)) {
+					cart = JSON.parse(localStorage.getItem(CART));
+				}
+				cart.push(`${book.title}`);
+				localStorage.setItem(CART, JSON.stringify(cart));
+
+				//elimina dal carrello
+				let deleteFromCart = document.createElement("button");
+				deleteFromCart.setAttribute("type", "button");
+				deleteFromCart.classList.add("btn");
+				deleteFromCart.classList.add("btn-outline-danger");
+				deleteFromCart.textContent = "Elimina";
+				item.appendChild(deleteFromCart);
+				deleteFromCart.addEventListener("click", () => {
+					item.remove();
+				});
+			});
+			row.appendChild(col);
+		});
+	})
+	.catch((error) => console.log("CATCH", error));
+
+//DISCARD BUTTON
